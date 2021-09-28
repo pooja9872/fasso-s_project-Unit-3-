fd9941836ea11a561cfc32f4743b0187f9417658
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-09-28 14:04:15 +0530

    first commit

--- fassos_projects/src/configs/db.js
+++ fassos_projects/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect("mongodb+srv://kiran:KiranFassos@cluster0.gant3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://kiran:KiranFassos@cluster0.gant3.mongodb.net/Fassos_project?retryWrites=true&w=majority")
 }
 
 module.exports = connect;
